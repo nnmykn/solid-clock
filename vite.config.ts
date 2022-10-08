c9ee96b4db44e5c02634c65383466d7509baa5d6
Ninomiya Kan <ninomiyakan.dev@gmail.com>
2022-10-08 11:18:42 +0900

    ビルド出来るように

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import solid from 'solid-start/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+import Icons from 'unplugin-icons/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [solid()],
-});
+    ssr: {
+        noExternal: ['solid-styled-components'],
+    },
+    plugins: [
+        Icons({
+            compiler: 'solid',
+            customCollections: {
+                routex: FileSystemIconLoader('./src/assets/icons'),
+            },
+        }),
+        solid({
+            adapter: 'solid-start-static',
+        }),
+    ],
+})
